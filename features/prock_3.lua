b02dfa33c9c7f7b4bfe7bce7811282785f4732f6
TurBoss <j.l.toledano.l@gmail.com>
2014-07-26 00:28:01 +0200

    Nuevas Features

---
+++ features/prock_3.lua
@@ -0,0 +1,24 @@
+local objectname= "prock_3" 
+local featureDef	=	{
+	name				= "prock_3",
+	world				="All Worlds",
+	description			="simple rock",
+	category			="Rocks",
+	object				="features/procks/prock_3.dae",
+	footprintx			=2,
+	footprintz			=2,
+	height				=30,
+	blocking			=true,
+	upright				=true,
+	hitdensity			= "5",
+	energy				=0,
+
+	damage				=10000,
+	flammable			=true,
+	reclaimable			=false,
+	autoreclaimable		=flase,
+	customparams = { 
+		randomrotate		= "true", 
+	}, 
+}
+return lowerkeys({[objectname] = featureDef}) 
