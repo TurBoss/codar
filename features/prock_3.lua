35120f3961b524fbb523f5bd07cd7159323c5ed0
PerroTron <perrotronn@gmail.com>
2014-07-27 02:29:44 +0200

    balance rocks

--- features/prock_3.lua
+++ features/prock_3.lua
@@ -13,7 +13,7 @@ local featureDef	=	{
 	hitdensity			= "5",
 	energy				=0,
 
-	damage				=10000,
+	damage				=5000,
 	flammable			=true,
 	reclaimable			=false,
 	autoreclaimable		=flase,
--- features/prock_7.lua
+++ features/prock_7.lua
@@ -13,7 +13,7 @@ local featureDef	=	{
 	hitdensity			= "5",
 	energy				=0,
 
-	damage				=5000,
+	damage				=2000,
 	flammable			=true,
 	reclaimable			=false,
 	autoreclaimable		=flase,
