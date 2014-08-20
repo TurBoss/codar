092f24504627eeb9d1585893b9ca1849cd14c28c
TurBoss <j.l.toledano.l@gmail.com>
2014-08-20 13:37:30 +0200

    map version

--- mapinfo.lua
+++ mapinfo.lua
@@ -8,7 +8,7 @@ local mapinfo = {
 	shortname   = "XllR",
 	description = "Porretown Map",
 	author      = "PerroTron",
-	version     = "0.3",
+	version     = "0.5",
 	--mutator   = "deployment";
 	--mapfile   = "", --// location of smf/sm3 file (optional)
 	modtype     = 3, --// 1=primary, 0=hidden, 3=map
