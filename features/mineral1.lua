269a9843f2e061bc46835dfb77f972ff0e6405a3
PerroTron <perrotronn@gmail.com>
2014-09-09 02:38:59 +0200

    Mineral balance

--- features/mineral1.lua
+++ features/mineral1.lua
@@ -11,7 +11,7 @@ local featureDef	=	{
 	blocking			=true,
 	upright				=true,
 	hitdensity			= "5",
-	metal				=240,
+	metal				=800,
 
 	damage				=4000,
 	flammable			=false,
--- features/mineral2.lua
+++ features/mineral2.lua
@@ -11,7 +11,7 @@ local featureDef	=	{
 	blocking			=true,
 	upright				=true,
 	hitdensity			= "5",
-	metal				=350,
+	metal				=1500,
 
 	damage				=8000,
 	flammable			=false,
