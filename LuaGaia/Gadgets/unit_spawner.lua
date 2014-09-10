104db05e0f97c187bd7c8bcc197483efe241e2a6
PerroTron <perrotronn@gmail.com>
2014-09-10 23:12:06 +0200

    new jungla

--- LuaGaia/Gadgets/unit_spawner.lua
+++ LuaGaia/Gadgets/unit_spawner.lua
@@ -9,6 +9,7 @@ function gadget:GetInfo()
     enabled   = true,  --  loaded by default?
   }
 end
+
 mineralID = {}
 
 SpawnTimer = {}
@@ -21,6 +22,9 @@ local gaiaTeamID = Spring.GetGaiaTeamID()
 if (not gadgetHandler:IsSyncedCode()) then
 	return false
 end
+
+------------------------------------------------------------------------
+------------------------------------------------------------------------
 
 function SpawnTimer.create(timeToSpawn)
 	local tim = {}             -- our new object
@@ -55,31 +59,53 @@ function SpawnTimer:update(gameFrame)
 	end
 end
 
+------------------------------------------------------------------------
+------------------------------------------------------------------------
+
 function gadget:Initialize ()	
 	if Game.gameName ~= "Jauria RTS"	then
 		Spring.Echo ("PLAY WITH Jauria RTS!!")
 		gadgetHandler:RemoveGadget()
 		return
 	end
+	
 	timer1 = SpawnTimer.create(8000) --timer dramon
 	timer2 = SpawnTimer.create(8000)
 	timer3 = SpawnTimer.create(8000)
 	timer4 = SpawnTimer.create(8000)
+	timer5 = SpawnTimer.create(8000)
+	timer6 = SpawnTimer.create(8000)
+	timer7 = SpawnTimer.create(8000)
+	timer8 = SpawnTimer.create(8000)
+	timer9 = SpawnTimer.create(8000)
+	timer10 = SpawnTimer.create(8000)
+	timer11 = SpawnTimer.create(8000)
+	
 	timerc5 = SpawnTimer.create(9000) --timer cofre
 	timerc6 = SpawnTimer.create(9000)
 	timerc7 = SpawnTimer.create(9000)
 end
 
 function gadget:GameStart()
-	timerc5:set_timer()
+	--timerc5:set_timer()
 	timerc6:set_timer()
 	timerc7:set_timer()
+	
 	spawnNPC(1)
 	spawnNPC(2)
 	spawnNPC(3)
 	spawnNPC(4)
+	spawnNPC(5)
+	spawnNPC(6)
+	spawnNPC(7)
+	spawnNPC(8)
+	spawnNPC(9)
+	spawnNPC(10)
+	spawnNPC(11)
+	
 	spawnTurrets1()
 	spawnTurrets2()
+	
 	--unselectableTurrents()
 end
 
@@ -92,6 +118,20 @@ function gadget:UnitDestroyed(unitID, unitDefID, unitTeam, attackerID, attackerD
 		timer3:set_timer()
 	elseif unitID == 10603 then
 		timer4:set_timer()
+	elseif unitID == 10604 then
+		timer5:set_timer()
+	elseif unitID == 10605 then
+		timer6:set_timer()
+	elseif unitID == 10606 then
+		timer7:set_timer()
+	elseif unitID == 10607 then
+		timer8:set_timer()
+	elseif unitID == 10608 then
+		timer9:set_timer()
+	elseif unitID == 10609 then
+		timer10:set_timer()
+	elseif unitID == 106010 then
+		timer11:set_timer()
 	end
 end
 
@@ -100,9 +140,18 @@ function gadget:GameFrame(n)
 	if timer2:update(n) then spawnNPC(2) end
 	if timer3:update(n) then spawnNPC(3) end
 	if timer4:update(n) then spawnNPC(4) end
+	if timer5:update(n) then spawnNPC(5) end
+	if timer6:update(n) then spawnNPC(6) end
+	if timer7:update(n) then spawnNPC(7) end
+	if timer8:update(n) then spawnNPC(8) end
+	if timer9:update(n) then spawnNPC(9) end
+	if timer10:update(n) then spawnNPC(10) end
+	if timer11:update(n) then spawnNPC(11) end
+	
 	if timerc5:update(n) then spawnFeature(1) end
 	if timerc6:update(n) then spawnFeature(2) end
 	if timerc7:update(n) then spawnFeature(3) end
+	
 	checkFeatures(1)
 	checkFeatures(2)
 	checkFeatures(3)
@@ -147,11 +196,19 @@ end
 function spawnNPC(i)
 	--Spring.Echo("Spawn Npc " .. i)
 	npc = {
-		{x=3260,name="gusano",z=4040,unitID=10600,rot="west",},
-		{x=850,name="destructor",z=2370,unitID=10602,rot="east",},
+		{name="gusano",x=3260,z=4040,unitID=10600,rot="west",},
+		{name="gusano",x=1870,z=1060,unitID=10601,rot="east",},
 		
-		{x=1870,name="gusano",z=1060,unitID=10601,rot="east",},
-		{x=4270,name="destructor",z=2725,unitID=10603,rot="west",},
+		{name="destructor",x=4885,z=4885,unitID=10602,rot="west",},
+		{name="destructor",x=4220,z=3210,unitID=10603,rot="east",},
+		
+		{name="escorpion",x=920,z=1920,unitID=10604,rot="west",},
+		{name="destructor",x=220,z=220,unitID=10605,rot="east",},
+		{name="destructor",x=620,z=3950,unitID=10606,rot="west",},
+		{name="escorpion",x=2025,z=4130,unitID=10607,rot="south",},
+		{name="destructor",x=4500,z=1160,unitID=10608,rot="east",},
+		{name="destructor",x=3080,z=985,unitID=10609,rot="north",},
+		{name="escorpion",x=2530,z=2530,unitID=10610,rot="east",},
 	}
 	Spring.CreateUnit (npc[i].name, npc[i].x, 100, npc[i].z, npc[i].rot, gaiaTeamID, false, true, npc[i].unitID)
 	--[[for i=1, #npc do
@@ -163,14 +220,14 @@ function spawnTurrets1()
 	--Spring.Echo("Spawns Turrents 1")
 	npc = {
 		--TOP--
-		{x=500,name="torreta",z=3310,unitID=10500,rot="east",},
-		{x=500,name="torreta",z=1510,unitID=10501,rot="east",},
+		{name="torreta",x=500,z=3310,unitID=10500,rot="east",},
+		{name="torreta",x=500,z=1510,unitID=10501,rot="east",},
 		--MID--
-		{x=1186,name="torreta",z=3915,unitID=10502,rot="east",},
-		{x=2091,name="torreta",z=3015,unitID=10503,rot="east",},
+		{name="torreta",x=1186,z=3915,unitID=10502,rot="east",},
+		{name="torreta",x=2091,z=3015,unitID=10503,rot="east",},
 		--BOT--
-		{x=1810,name="torreta",z=4620,unitID=10504,rot="east",},
-		{x=3610,name="torreta",z=4620,unitID=10505,rot="east",},
+		{name="torreta",x=1810,z=4620,unitID=10504,rot="east",},
+		{name="torreta",x=3610,z=4620,unitID=10505,rot="east",},
 	}
 	for i=1, #npc do
 		Spring.CreateUnit (npc[i].name, npc[i].x, 100, npc[i].z, npc[i].rot, 0, false, true, npc[i].unitID)
@@ -181,14 +238,14 @@ function spawnTurrets2()
 	--Spring.Echo("Spawns Turrents 2")
 	npc = {
 		--BOT--
-		{x=3315,name="torreta",z=500,unitID=10506,rot="west",},
-		{x=1520,name="torreta",z=500,unitID=10507,rot="west",},
+		{name="torreta",x=3315,z=500,unitID=10506,rot="west",},
+		{name="torreta",x=1520,z=500,unitID=10507,rot="west",},
 		--MID--
-		{x=3935,name="torreta",z=1186,unitID=10508,rot="west",},
-		{x=3020,name="torreta",z=2105,unitID=10509,rot="west",},
+		{name="torreta",x=3935,z=1186,unitID=10508,rot="west",},
+		{name="torreta",x=3020,z=2105,unitID=10509,rot="west",},
 		--TOP--
-		{x=4628,name="torreta",z=1810,unitID=10510,rot="west",},
-		{x=4628,name="torreta",z=3610,unitID=10511,rot="west",},
+		{name="torreta",x=4628,z=1810,unitID=10510,rot="west",},
+		{name="torreta",x=4628,z=3610,unitID=10511,rot="west",},
 	}
 	for i=1, #npc do
 		Spring.CreateUnit (npc[i].name, npc[i].x, 100, npc[i].z, npc[i].rot, 1, false, true, npc[i].unitID)
